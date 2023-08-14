a3fb933073cff8c7f582e1455a5060b99303e9ac
Nathan Phipps O'Neill <nathan.phipps.oneill@davra.com>
2023-08-14 22:46:25 +0100

    Add flag for z offset

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,35 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "string.h"
 #include "custom_time.h"
 #include "config.h"
 #include "logger.h"
 #include "AccelerometerProcessor.h"
 
-int main() {
+int main(int argc, char** argv)
+{
     Logger::EnableFileOutput(LOGFILENAME);
     Logger::SetPriority(LogPriority::TracePriority);
     Logger::Info("Starting accelerometer reading...");
 
     // Create an instance of the AccelerometerProcessor class
-    AccelerometerProcessor processor;
-    processor.processAccelerometerData(SAMPLE_DATA_FILENAME);
+    AccelerometerProcessor *processor = new AccelerometerProcessor();
 
+    int opt;
+    while ((opt = getopt(argc, argv, "z:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'z':
+            processor->enableZOffset(true);
+            break;
+        default:
+            // Handle invalid options
+            break;
+        }
+    }
+
+    processor->processAccelerometerData(SAMPLE_DATA_FILENAME);
 
     return 0;
 }
